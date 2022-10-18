2b0e59362a942078c11c5aa3d2564dc3f444d9b0
lsw4701 <pro_4701@naver.com>
2022-10-18 18:19:53 +0900

    df

--- apne2/prod/eks-irsa/_backend.tf
+++ apne2/prod/eks-irsa/_backend.tf
@@ -1,9 +1,12 @@
 terraform {
   backend "s3" {
-    bucket = "devops-prod-tfbackend-s3"
+    bucket = "lsw-bk"
     key    = "apne2/eks-irsa/terraform.tfstate"
     region = "ap-northeast-2"
     #    role_arn    = "{ASSUMED_ROLE}"
     max_retries = 3
   }
 }
+
+
+
--- apne2/prod/eks/_backend.tf
+++ apne2/prod/eks/_backend.tf
@@ -1,9 +1,10 @@
 terraform {
 
-  
+
   backend "s3" {
-    bucket = "lsw-bucket"             #   버켓명 
+    bucket = "lsw-bk"             #   버켓명 
     key = "terra2/terraform.tfstate"  #  생성되는 폴더/파일명  
+    role_arn    = "arn:aws:iam::959714228357:role/terraformAsume"
     region = "ap-northeast-2"
   }
 

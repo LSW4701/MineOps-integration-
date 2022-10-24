2f406deb6fbd96148b2eda52f3f20d10b6216a6d
lsw4701 <pro_4701@naver.com>
2022-10-24 19:23:18 +0900

    fd

--- env/ec2/ec2-instance/ec2.tf
+++ env/ec2/ec2-instance/ec2.tf
@@ -14,6 +14,23 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+locals {
+  openvpn_userdata = templatefile("${path.module}/files/userdata.sh", {
+    vpc_cidr  = local.vpc.cidr_block
+    public_ip = aws_eip.openvpn.public_ip
+  })
+  
+
+  jenkins_userdata = templatefile("${path.module}/files/jenkin_userdata.sh", {
+    vpc_cidr  = local.vpc.cidr_block
+    public_ip = aws_eip.openvpn.public_ip
+  })
+  
+  common_tags = {
+    "Project" = "utill"
+  }
+}
+
 resource "aws_instance" "private" {
   ami           = data.aws_ami.ubuntu.image_id
   instance_type = "t3.small" #
@@ -29,26 +46,10 @@ resource "aws_instance" "private" {
   }
 }
 
-locals {
-  openvpn_userdata = templatefile("${path.module}/files/userdata.sh", {
-    vpc_cidr  = local.vpc.cidr_block
-    public_ip = aws_eip.openvpn.public_ip
-  })
-  
-
-  jenkins_userdata = templatefile("${path.module}/files/jenkin_userdata.sh", {
-    vpc_cidr  = local.vpc.cidr_block
-    public_ip = aws_eip.openvpn.public_ip
-  })
-  
-  common_tags = {
-    "Project" = "openvpn"
-  }
-}
 
 resource "aws_instance" "openvpn" {
   ami           = data.aws_ami.ubuntu.image_id
-  instance_type = "t2.micro"
+  instance_type = "t3.small"
   subnet_id     = local.subnet_groups["public"].ids[0]
   key_name      = "linux_s" ##
 
@@ -76,10 +77,29 @@ resource "aws_instance" "jenkins" {
   associate_public_ip_address = false
   vpc_security_group_ids = [
     module.sg__ssh.id,
-    module.sg__openvpn.id,
+    module.sg__jenkins.id,
   ]
 
   tags = {
     Name = "${local.vpc.name}-jenkins"
+  }
+}
+
+resource "aws_instance" "argo" {
+  ami           = data.aws_ami.ubuntu.image_id
+  instance_type = "t3.small"
+  subnet_id     = local.subnet_groups["public"].ids[0]
+  key_name      = "linux_s" ##
+
+  user_data = local.jenkins_userdata
+
+  associate_public_ip_address = false
+  vpc_security_group_ids = [
+    module.sg__ssh.id,
+    module.sg__argo.id,
+  ]
+
+  tags = {
+    Name = "${local.vpc.name}-argo"
   }
 }

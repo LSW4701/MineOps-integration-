4ea8d066b7bf2d134f5339e19d7eb5843090e274
lsw4701 <pro_4701@naver.com>
2022-10-24 12:15:54 +0900

    df

--- env/ec2/jenkins/_remote.tf
+++ env/ec2/jenkins/_remote.tf
@@ -1,10 +1,10 @@
-locals {
-  remote_states = {
-    "network" = data.terraform_remote_state.network.outputs
-  }
-  vpc           = local.remote_states["network"].vpc
-  subnet_groups = local.remote_states["network"].subnet_groups
-}
+# locals {
+#   remote_states = {
+#     "network" = data.terraform_remote_state.network.outputs
+#   }
+#   vpc           = local.remote_states["network"].vpc
+#   subnet_groups = local.remote_states["network"].subnet_groups
+# }
 
 
 

8f33471c91a0d4029035abff73e1b19f75042bac
lsw4701 <pro_4701@naver.com>
2022-10-24 11:43:38 +0900

    df

--- env/ec2/jenkins/_backend.tf
+++ env/ec2/jenkins/_backend.tf
@@ -4,7 +4,7 @@ terraform {
     bucket = "lsw-bbk"             #   버켓명 
     key = "apne2/ec2/jenkins/terraform.tfstate"  #  생성되는 폴더/파일명  
     role_arn    = "arn:aws:iam::959714228357:role/terraformAsume"
-    region = "ap-northeast-2"
+    region = "ap-northeast-2" 
   }
 }
 

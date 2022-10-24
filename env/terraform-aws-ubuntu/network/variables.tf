79db0288d07fcea50b8b9bbffa9116e2b0867195
lsw4701 <pro_4701@naver.com>
2022-10-24 10:53:09 +0900

    gf

--- env/terraform-aws-ubuntu/network/variables.tf
+++ env/terraform-aws-ubuntu/network/variables.tf
@@ -4,11 +4,5 @@ variable "config_file" {
   default     = "./config.yaml"
 }
 
-variable "vpc_flow_retention_days" {}
-variable "vpc_flow_log_format" {
-  description = "Private VPC flow log format"
-  default     = "$${version} $${account-id} $${interface-id} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${protocol} $${packets} $${bytes} $${start} $${end} $${action} $${log-status}"
-#  default     = "$${version} $${vpc-id} $${subnet-id} $${instance-id} $${interface-id} $${account-id} $${type} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${pkt-srcaddr} $${pkt-dstaddr} $${protocol} $${bytes} $${packets} $${start} $${end} $${action} $${tcp-flags} $${log-status}"
-}
 
 vpc_flow_retention_days = 1
